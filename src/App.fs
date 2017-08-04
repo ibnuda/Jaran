5df8b3523fa8188db99637501759cf90730293a3
Ibnu Daru Aji <ibnu.d.a@student.uns.ac.id>
2017-08-04 20:51:18 +0700

    Init commit.

---
+++ src/App.fs
@@ -0,0 +1,28 @@
+module Jaran
+
+open System
+
+open Fable.Core
+open Fable.Core.JsInterop
+open Fable.Import
+
+type Something =
+    { lastuser : string
+      lastactive : int64 }
+
+let u = """ { "lastuser": "Administrator", "lastactive": 636372711333291008 } """
+
+let init() =
+    Browser.console.debug (sprintf "this : %A" <| ofJson<Something> u)
+    let canvas = Browser.document.getElementsByTagName_canvas().[0]
+    canvas.width <- 1000.
+    canvas.height <- 800.
+    let ctx = canvas.getContext_2d()
+    // The (!^) operator checks and casts a value to an Erased Union type
+    // See http://fable.io/docs/interacting.html#Erase-attribute
+    ctx.fillStyle <- !^"rgb(200,0,0)"
+    ctx.fillRect (10., 10., 55., 50.)
+    ctx.fillStyle <- !^"rgba(0, 0, 200, 0.5)"
+    ctx.fillRect (30., 30., 55., 50.)
+
+init()
